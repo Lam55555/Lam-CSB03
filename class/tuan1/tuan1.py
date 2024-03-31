d5f167793e883c3baef9359ce8d89a0f37b92a56
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-03-31 11:52:01 +0700

    baidau

---
+++ class/tuan1/tuan1.py
@@ -0,0 +1,42 @@
+from turtle import *
+pen
+# # left(90)
+# forward(200)
+# right(90)
+# forward(200)
+# right(90)
+# forward(200)
+# right(90)
+# forward(200)
+# # right(90)
+# # backward(90)
+# mainloop()
+
+# left(180)
+# forward(100)
+# right(70)
+# forward(100)
+# right(70)
+# forward(100)
+# right(70)
+# forward(100)
+# right(70)
+# forward(100)
+
+right(0)
+forward(200)
+left(36)
+backward(200)
+left(36)
+forward(200)
+left(36)
+backward(200)
+left(36)
+forward(200)
+
+mainloop()
+
+
+# x = 5
+# y = 'Hello World'
+# print(y)
