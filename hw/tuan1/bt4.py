18c6d010a0002d2a5a637131aed2a57f570389bd
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-04-06 16:29:08 +0700

    a

--- hw/tuan1/bt4.py
+++ hw/tuan1/bt4.py
@@ -1,4 +1,6 @@
-# year = int(input("What year is it?\n"))
-# if len(str(year)) == 4:
-#     print('HAPPY NEW YEAR\n!!!',year,'!!!')
-print("\t   . : .   \n\t '.  :  .'\n        .  '.:.'  .\n        .  .':'.  .\n         .'  :  '.\n           ' : '")
+year = int(input("What year is it?\n"))
+if len(str(year)) == 4:
+    print('HAPPY NEW YEAR\n!!!',year,'!!!')
+    print("\t   . : .   \n\t '.  :  .'\n        .  '.:.'  .\n        .  .':'.  .\n         .'  :  '.\n           ' : '")
+else:
+    print("y/c 4 số")
