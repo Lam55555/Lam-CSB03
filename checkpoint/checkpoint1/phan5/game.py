7ce3cbd7b6a5037d0a7c03d2360ca48bb19e6dd5
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-04-22 13:11:42 +0700

    pe

--- checkpoint/checkpoint1/phan5/game.py
+++ checkpoint/checkpoint1/phan5/game.py
@@ -3,8 +3,8 @@ diem = 0
 while True:
     diem +=1
     # score = 0
-    a = random.randint(-10,10)
-    b = random.randint(-10,10)
+    a = random.randint(0,20)
+    b = random.randint(0,20)
     operator = ['+','-','*','/']
     opval = random.choice(operator)
     if opval == '+':
@@ -12,79 +12,98 @@ while True:
         gacha = random.randint(n-1,n+1)
         print('kq cua', a,'+',b, 'la',gacha)
         kq = int(input('1 = true, 0 = False:  '))
-        if kq == 1:
-            if gacha == n:
-                print('true')
-                diem +1
-            else:
-                print('false')
-                break
+        if kq == None or  kq>1 or kq<0:
+            print('error')
+            break
         else:
-            if gacha != n:
-                print('true')
-                diem +1
+            if kq == 1:
+                if gacha == n:
+                    print('true')
+                    diem +1
+                else:
+                    print('false')
+                    break
             else:
-                print('false')
-                break
+                if gacha != n:
+                    print('true')
+                    diem +1
+                else:
+                    print('false')
+                    break
     elif opval == '-':
         n = a-b
         gacha = random.randint(n-1,n+1)
         print('kq cua', a,'-',b, 'la',gacha)
         kq = int(input('1 = true, 0 = False:  '))
-        if kq == 1:
-            if gacha == n:
-                print('true')
-                diem +1
-                
+        if kq == None or  kq>1 or kq<0:
+            print('error')
+            break
+        else: 
+            if kq == 1:
+                if gacha == n:
+                    print('true')
+                    diem +1
+
+                else:
+                    print('false')
+                    break
             else:
-                print('false')
-                break
-        else:
-            if gacha != n:
-                print('true')
-                diem +1
-                
-            else:
-                print('false')
-                break
-    if opval == '*':
+                if gacha != n:
+                    print('true')
+                    diem +1
+
+                else:
+                    print('false')
+                    break
+    elif opval == '*':
         n = a*b
         gacha = random.randint(n-1,n+1)
         print('kq cua', a,'*',b, 'la',gacha)
         kq = int(input('1 = true, 0 = False:  '))
-        if kq == 1:
-            if gacha == n:
-                print('true')
-                diem +1
+        if kq == None or  kq>1 or kq<0:
+            print('error')
+            break
+        else: 
+            if kq == 1:
+                if gacha == n:
+                    print('true')
+                    diem +1
+                else:
+                    print('false')
+                    break
             else:
-                print('false')
-                break
-        else:
-            if gacha != n:
-                print('true')
-                diem +1
-            else:
-                print('false')
-                break
-    if opval == '/':
+                if gacha != n:
+                    print('true')
+                    diem +1
+                else:
+                    print('false')
+                    break
+    elif opval == '/':
         while b == 0:
-            b = random.randint(-1,1)
+            b = random.randint(0,20)
+        else:
             n = a/b
             c = int(n)
             gacha = random.randint(c-1,c+1)
             print('kq cua', a,'/',b, 'la',gacha)
             kq = int(input('1 = true, 0 = False:  '))
-            if kq == 1:
-                if gacha == c:
-                    print('true')
-                    diem +1
+            if kq == None or  kq>1 or kq<0:
+                print('error')
+                break
+            else: 
+                if kq == 1:
+                    if gacha == c:
+                        print('true')
+                        diem +1
+                    else:
+                        print('false')
                 else:
-                    print('false')
-            else:
-                if gacha != c:
-                    print('true')
-                    diem +1                 
-                else:
-                    print('false')
-                    break
+                    if gacha != c:
+                        print('true')
+                        diem +1                 
+                    else:
+                        print('false')
+                        break
 print("diem cua ban la",diem-1)
+
+# <("")
