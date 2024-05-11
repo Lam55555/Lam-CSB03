93ee533fc90c8d4902332d299152741643b10241
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-05-11 17:05:36 +0700

    a

--- hw/tuan5/p2/bt3.py
+++ hw/tuan5/p2/bt3.py
@@ -1,8 +1,6 @@
-number = int(input('Input a positive number: '))
-fib = [0]*(number + 1)
+n = int(input("Input a positive number: "))
+fib = [0]*(n + 1)
 fib[1] = 1
-print(fib)
-for i in range(2,number+1):
-    print(i, end='')
+for i in range(2, n + 1):
     fib[i] = fib[i - 1] + fib[i - 2]
-print("First",number,"Fibonacci number(s):",*fib[1:])
+print("First",n,"Fibonacci number(s):", *fib[1:])
