b4ff179e00bfcb2f8fed5c64c2aec591ecbf90a9
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-05-24 07:25:19 +0700

    t6

--- hw/tuan6/bai2.py
+++ hw/tuan6/bai2.py
@@ -7,7 +7,7 @@ def snt(x):      #x=11
             return False
     return True                 #11/3 !=0 -> la so ngto
 number = int(input('Print a number: '))
-if snt(number) == True:
+if snt(number):
     print('la so nguyen to')
 else:
     print('khong phai so nguyen to')
