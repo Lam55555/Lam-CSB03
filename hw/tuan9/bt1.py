5b203b140f3ae776d4e70bc9f91b1623f5f2b361
Lam55555 <nguyenhoanglam121106@gmail.com>
2024-06-09 11:46:03 +0700

    mini4

--- hw/tuan9/bt1.py
+++ hw/tuan9/bt1.py
@@ -1,9 +1,12 @@
 numbers = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5,
          'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9, 'X': 10}
 
-n = input('Input: ')
-if n in numbers:
-    print(numbers[n])
-else:
-    print('Not found')
+
+def solama(n):
+    if n in numbers:
+        return print(numbers[n])
+    else:
+        return print('Khong tim thay')
+y = str(input('Input: '))  
+solama(y)  
         
---
+++ mini4.py
@@ -0,0 +1,115 @@
+# 1
+pc = {
+    'HP': 20,
+    'DELL': 50,
+    'MACBOOK': 12,
+    'ASUS': 30,
+}
+
+# print(f"Available Macbooks: {pc['MACBOOK']}")
+# name = input('Input a brand: ')
+# print(f"Available {name}s: {pc[name]}")
+
+# 2.1-2.2
+
+# pc["TOSHIBA"] = "10"
+# brand = str(input('Input a brand: '))
+# amount = int(input('Input a mount: '))
+# pc[brand] = amount
+
+# for i in pc:
+#     print(f"- {i}: {pc[i]}")
+
+# 2.3
+
+# pc['DELL'] = 60
+# pc['MACBOOK'] = 2
+
+# for i in pc:
+#     print(f"- {i}: {pc[i]}")
+
+# 2.4
+
+# tong = 0
+# for i in pc.values():
+#     tong+=i
+# print('Total product:',tong)
+
+# 3
+
+pcprice = {
+    'HP':600,
+    'DELL':650,
+    'MACBOOK':1200,
+    'ASUS':400
+}
+
+# print(f"Price of Asus: {pcprice['ASUS']}")
+# price = input('Input a brand: ')
+# print(f"Price of {price}: {pcprice[price]}")
+
+# 4.1
+# print('Total price:',pcprice['ASUS']*5)
+
+# 4.2
+
+# brand = input('Input a brand: ')
+# amountPc = int(input('Input amount to buy: '))
+# print(f"Total price: {pcprice[brand]*amountPc}")
+
+# 4.3
+# brand = input('Input a brand: ')
+# amountPc = int(input('Input amount to buy: '))
+# print(f"Total price: {pcprice[brand]*amountPc}")
+
+# print('Available product: ')
+# if brand in pc:
+#     pc[brand] = pc[brand] - amountPc
+# for i in pc:
+#     print(f"- {i}: {pc[i]}")
+
+# 5
+# hang  = {}
+# print('Total price of avalable brand: ')
+# for i in pcprice:
+#     if i in pc:
+#         hang[i] = pc[i]*pcprice[i]
+# for i in hang:
+#     print(f"-{i}: {hang[i]}")
+
+# print('Total value of availabel items:',sum(hang.values()))
+
+char = {
+    'Name': 'Light',
+    'Age': 17,
+    'Strength': 8,
+    'Defense': 10,
+    'HP': 100,
+    'Backpack': 'Shield, Bread Loaf',
+    'Gold': 100,
+    'Level': 2,
+}
+#6
+# char['Gold']+=50
+# print('Gold',char['Gold'])
+
+# char.update({'Backpack':char['Backpack']+(', FlintStone')})
+# print('Backpack:',char['Backpack'])
+
+
+skill = {
+  'Skill 1': [
+    {'Name': 'Tackle', 'Minimum level': 1, 'Damage': 5, 'Hit rate': 0.3 },
+  ],
+  'Skill 2': [
+    {'Name': 'Quick Attack','Minimum level': 2, 'Damage': 3, 'Hit rate': 0.5},
+  ],
+  'Skill 3':[
+      {'Name': 'Strong Kick', 'Minimum level': 4, 'Damage': 9, 'Hit rate': 0.2}
+  ]
+}
+#7
+for i in skill:
+    for y in skill[i]:
+        print(f"{i}: {y['Name']}")
+
